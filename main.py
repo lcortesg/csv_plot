ac19b25fa0f45707406fc51c7540ce8746000570
Lucas Cortés <lucas.cortes.14@sansano.usm.cl>
2022-04-29 10:34:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
 
     st.title("CSV Plot")
 
-    uploaded_files = st.file_uploader("Hola Nico! elige un archivo CSV", accept_multiple_files=True, on_change=main, help="Selecciona uno o más archivos CSV para graficar")
+    uploaded_files = st.file_uploader("Hola Nico! elige un archivo CSV", accept_multiple_files=True, help="Selecciona uno o más archivos CSV para graficar")
     #if uploaded_files is not None:
     if len(uploaded_files) > 0:
         options = st.multiselect(
